d261e13dedc18d863807f5f7ba823d697e2e3bdb
Arturo Guillén <aguillen.js@gmail.com>
2024-02-02 11:32:12 -0400

    Fully added items creation usin the API. Initial implementation of item deletion and item overriding.

--- app/Livewire/OpportunityItemsCreate.php
+++ app/Livewire/OpportunityItemsCreate.php
@@ -25,9 +25,13 @@ class OpportunityItemsCreate extends Component
     #[Locked]
     public array $job = [];
 
+    #[Locked]
+    public int $opportunityId = 0;
+
     public function mount(array $job): void
     {
         $this->job = $job;
+        $this->opportunityId = App::environment(['local', 'staging']) ? config('app.mph_test_opportunity_id') : $this->job['id'];
     }
 
     public function save(): mixed
@@ -40,37 +44,50 @@ class OpportunityItemsCreate extends Component
 
         $this->csvfile->storeAs(path: 'csv_files', name: $filename);
 
-        $data = [];
-        $header = null;
+        $items = [];
+        $headings = null;
         $requiredHeadings = ['id', 'quantity', 'group_name'];
 
         if (($handle = fopen(base_path() . '/storage/app/csv_files/' . $filename, "r")) !== FALSE) {
             while (($row = fgetcsv($handle, 1000, ",")) !== FALSE) {
-                if ($header === null) {
-                    $header = $row;
+                if ($headings === null) {
+                    $headings = $row;
 
                     // VALIDATE HEADINGS
-                    $diff = array_diff($requiredHeadings, $header);
+                    $diff = array_diff($requiredHeadings, $headings);
 
                     if (count($diff) > 0) {
                         fclose($handle);
-                        $missingHeaders = Arr::join($diff, ', ', ' and ');
-                        throw ValidationException::withMessages(['csvfile' => __('The uploaded csv file does not contain the valid headers required to identify the data. Missing headers: :missing_headers.', ['missing_headers' => $missingHeaders])]);
+                        $missingHeadings = Arr::join($diff, ', ', ' and ');
+                        throw ValidationException::withMessages(['csvfile' => __('The uploaded csv file does not contain the valid headings required to identify the items. Missing headings: :missing_headings.', ['missing_headings' => $missingHeadings])]);
                     }
 
                     continue;
                 }
 
-                $data[] = array_combine($header, $row);
+                $items[] = array_combine($headings, $row);
             }
             fclose($handle);
         }
 
         // EXTRACT EXISTING GROUPS
-        $existingGroups = array_values(array_map(fn ($group) => ['id' => $group['id'], 'name' => $group['name']], array_filter($this->job['opportunity_items'], fn ($item) => $item['opportunity_item_type_name'] === 'Group')));
+        $existingGroups = array_values(
+            array_map(
+                fn ($group) => ['id' => $group['id'], 'name' => $group['name']],
+                array_filter(
+                    $this->job['opportunity_items'],
+                    fn ($item) => $item['opportunity_item_type_name'] === 'Group'
+                )
+            )
+        );
 
         // EXTRACT GROUPS SENT OVER THE FILE IN A DISTINCT WAY
-        $groups = array_unique(array_map(fn ($item) => $item['group_name'], $data));
+        $groups = array_unique(
+            array_map(
+                fn ($item) => $item['group_name'],
+                $items
+            )
+        );
 
         // CHECK IF GROUPS SENT OVER THE FILE ARE PRESENT IN THE EXISTING GROUPS FROM THE JOB
         $groupDiff = array_diff(
@@ -96,44 +113,14 @@ class OpportunityItemsCreate extends Component
         }
 
         // CREATE OPPORTUNITY ITEMS
-        $this->createItems($data, $existingGroups);
+        $this->storeItems($items, $existingGroups);
 
-        dd($data);
-    }
-
-    protected function createItems(array $items, array $groups)
-    {
-        $opportunityId = App::environment(['local', 'staging']) ? config('app.mph_test_opportunity_id') : $this->job['id'];
-        $client = new Client(['base_uri' => config('app.current_rms.host')]);
-        $promises = array_map(function ($item) use ($client, $opportunityId, $groups) {
-            $headers = [
-                'X-AUTH-TOKEN' => config('app.current_rms.auth_token'),
-                'X-SUBDOMAIN' => config('app.current_rms.subdomain'),
-            ];
-
-            $query = [
-                'opportunity_item' => [
-                    'opportunity_id' => $opportunityId,
-                    'item_id' => $item['id'],
-                    'quantity' => $item['quantity'],
-                    'parent_opportunity_item_id' => 84539,
-                ],
-            ];
-
-            return $client->postAsync("opportunities/{$opportunityId}/opportunity_items", [
-                'headers' => $headers,
-                'query' => $query,
-            ]);
-        }, $items);
-
-        $responses = Promise\Utils::settle($promises)->wait();
-
-        return $responses;
+        dd('Success');
     }
 
     protected function createGroups(array $groups): array
     {
-        $opportunityId = App::environment(['local', 'staging']) ? config('app.mph_test_opportunity_id') : $this->job['id'];
+        $opportunityId = $this->opportunityId;
         $client = new Client(['base_uri' => config('app.current_rms.host')]);
         $promises = array_map(function ($group) use ($client, $opportunityId) {
             $headers = [
@@ -155,6 +142,71 @@ class OpportunityItemsCreate extends Component
                 'query' => $query,
             ]);
         }, $groups);
+
+        $responses = Promise\Utils::settle($promises)->wait();
+
+        return $responses;
+    }
+
+    protected function storeItems(array $items, array $groups = [])
+    {
+        $opportunityId = $this->opportunityId;
+        ['opportunity_items' => $opportunityItems] = $this->job;
+        $client = new Client(['base_uri' => config('app.current_rms.host')]);
+        $promises = [];
+
+        $promises = array_map(function ($item) use ($client, $opportunityId, $opportunityItems, $groups) {
+            // PREPARE ITEM DATA
+            [
+                'id' => $id,
+                'quantity' => $quantity,
+                'group_name' => $groupName,
+            ] = $item;
+
+            $id = intval($id);
+            $quantity = intval($quantity);
+
+            $headers = [
+                'X-AUTH-TOKEN' => config('app.current_rms.auth_token'),
+                'X-SUBDOMAIN' => config('app.current_rms.subdomain'),
+            ];
+
+            // CHECK IF ITEM ALREADY EXISTS IN JOB
+            $itemIds = array_column($opportunityItems, 'item_id');
+            $index = array_search($id, $itemIds);
+
+            dd($index, $opportunityItems[$index]);
+
+            if ($quantity <= 0) {
+                return $client->deleteAsync("opportunities/{$opportunityId}/opportunity_items/{$id}", ['headers' => $headers]);
+            }
+
+            $query = [
+                'opportunity_item' => [
+                    'opportunity_id' => $opportunityId,
+                    'item_id' => $id,
+                    'quantity' => $quantity,
+                    'price' => 0,
+                ],
+            ];
+
+            // GET GROUP ID TO BE ADDED TO
+            $groupIds = array_values(array_map(function ($group) {
+                return $group['id'];
+            }, array_filter($groups, function ($group) use ($groupName) {
+                return $group['name'] === $groupName;
+            })));
+
+            if (count($groupIds) > 0) {
+                [$groupId] = $groupIds;
+                $query['opportunity_item']['parent_opportunity_item_id'] = $groupId;
+            }
+
+            return $client->postAsync("opportunities/{$opportunityId}/opportunity_items", [
+                'headers' => $headers,
+                'query' => $query,
+            ]);
+        }, $items);
 
         $responses = Promise\Utils::settle($promises)->wait();
 
