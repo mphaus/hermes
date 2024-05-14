3664810d28980778ce3c34baeff3efe3bf74fb8a
Arturo Guillén <aguillen.js@gmail.com>
2024-05-14 08:29:16 -0400

    Discussions: added the comments column to the discussion_mappings table.

---
+++ database/migrations/2024_05_14_222454_update_discussion_mappings_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('discussion_mappings', function (Blueprint $table) {
+            $table->longText('comments')->after('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('discussion_mappings', function (Blueprint $table) {
+            $table->dropColumn('comments');
+        });
+    }
+};
