703e72638eba711a6c5131ab5c776a43afdf6829
Arturo Guillén <aguillen.js@gmail.com>
2024-05-14 11:35:10 -0400

    Discussions: added json file validation.

--- app/Livewire/UploadDiscussionMappings.php
+++ app/Livewire/UploadDiscussionMappings.php
@@ -3,6 +3,9 @@
 namespace App\Livewire;
 
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 use Livewire\Attributes\Validate;
 use Livewire\Component;
 use Livewire\WithFileUploads;
@@ -11,7 +14,7 @@ class UploadDiscussionMappings extends Component
 {
     use WithFileUploads;
 
-    #[Validate('required')]
+    // #[Validate('required')]
     public string $comments;
 
     #[Validate('file', message: 'This field must be a file.')]
@@ -22,6 +25,26 @@ class UploadDiscussionMappings extends Component
     public function save()
     {
         $this->validate();
+
+        $filename = $this->jsonfile->store();
+        $mappings = Storage::json($filename);
+
+        $mappingsValidator = Validator::make($mappings, [
+            '*' => Rule::forEach(function (array $value, string $attribute) {
+                $includeOpportunityOwnerAsParticipant = $value['include_opportunity_owner_as_participant'];
+
+                return [
+                    'title' => 'required',
+                    'first_message' => 'required',
+                    'include_opportunity_owner_as_participant' => 'required|boolean',
+                    'participants' => ['array', Rule::requiredIf(fn () => $includeOpportunityOwnerAsParticipant === false)],
+                    'participants.*.id' => ['numeric', Rule::requiredIf(fn () => $includeOpportunityOwnerAsParticipant === false)],
+                    'participants.*.full_name' => Rule::requiredIf(fn () => $includeOpportunityOwnerAsParticipant === false),
+                ];
+            }),
+        ]);
+
+        dd($mappingsValidator->fails());
     }
 
     public function render(): View
