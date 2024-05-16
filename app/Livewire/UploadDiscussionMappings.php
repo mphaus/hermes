bdf1132e81cc7557be1bf62e16199429f557bd9c
Arturo Guillén <aguillen.js@gmail.com>
2024-05-16 09:47:26 -0400

    Discussions: added verification of the existence of the participants indicated in the JSON file.

--- app/Livewire/UploadDiscussionMappings.php
+++ app/Livewire/UploadDiscussionMappings.php
@@ -2,8 +2,10 @@
 
 namespace App\Livewire;
 
+use App\Traits\WithHttpCurrentError;
 use Illuminate\Contracts\View\View;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
@@ -14,6 +16,7 @@ use Livewire\WithFileUploads;
 class UploadDiscussionMappings extends Component
 {
     use WithFileUploads;
+    use WithHttpCurrentError;
 
     // #[Validate('required')]
     public string $comments;
@@ -51,6 +54,41 @@ class UploadDiscussionMappings extends Component
             }
 
             return;
+        }
+
+        $participantsIds = array_unique(Arr::flatten(array_reduce($mappings, function ($carry, $mapping) {
+            if (empty($mapping['participants']) === false) {
+                $carry[] = array_map(fn ($participant) => $participant['id'], $mapping['participants']);
+            }
+
+            return $carry;
+        }, [])));
+
+        $queryParams = preg_replace('/\[\d+\]/', '[]',  urldecode(http_build_query([
+            'q[active_eq]' => true,
+            'q[id_in]' => $participantsIds,
+        ])));
+
+        $response = Http::current()->get("members?{$queryParams}");
+
+        if ($response->failed()) {
+            $this->addError('jsonfile', $this->errorMessage(__('An unexpected error occurred while ingesting the JSON file. Please refresh the page and try again.'), $response->json()));
+
+            return;
+        }
+
+        ['members' => $members] = $response->json();
+
+        $memberIds = array_map(fn ($member) => $member['id'], $members);
+        $diffIds = array_diff($participantsIds, $memberIds);
+
+        if (empty($diffIds) === false) {
+            session()->flash('alert', [
+                'type' => 'danger',
+                'message' => __('The ingestion failed. Either the JSON is not valid, or the users do not match those listed in CurrentRMS.'),
+            ]);
+
+            return $this->redirectRoute(name: 'discussions.edit', navigate: true);
         }
     }
 
