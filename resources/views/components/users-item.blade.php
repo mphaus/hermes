88e237956a374c87ad24cfc6e73c472c6405be90
Arturo Guillén <aguillen.js@gmail.com>
2024-10-08 12:09:28 -0400

    Users: added emojis to the users list to indicate if users are enabled and/or are admins.

--- resources/views/components/users-item.blade.php
+++ resources/views/components/users-item.blade.php
@@ -5,7 +5,7 @@
     x-on:hermes:user-delete.window="userIsBeingDeleted = true" 
     x-bind:class="{ 'pointer-events-none opacity-50': userIsBeingDeleted }"
 >
-    <div class="grid gap-4 lg:grid-cols-10">
+    <div class="grid gap-4 lg:grid-cols-10 lg:items-center">
         <div class="grid gap-1 lg:block lg:col-span-2">
             <p class="text-sm font-semibold lg:hidden">{{ __('Name') }}</p>
             <a href="{{ route('users.show', ['user' => $user['id']]) }}" wire:navigate title="{{ $user->full_name }}">{{ $user->full_name }}</a>
@@ -20,11 +20,21 @@
         </div>
         <div class="grid gap-1 text-sm lg:block">
             <p class="font-semibold lg:hidden">{{ __('Is admin') }}</p>
-            <p>{{ $user->is_admin ? __('Yes') : __('No') }}</p>
+            @if ($user->is_admin)
+                <div>
+                    <x-icon-flexed-biceps class="w-5 h-5" />
+                </div>
+            @endif
         </div>
         <div class="grid gap-1 text-sm lg:block">
             <p class="font-semibold lg:hidden">{{ __('Is enabled') }}</p>
-            <p>{{ $user->is_enabled ? __('Yes') : __('No') }}</p>
+            <div>
+                @if ($user->is_enabled)
+                    <x-icon-check-mark-button class="w-5 h-5" />
+                @else
+                    <x-icon-heavy-multiplication-x class="w-5 h-5" />
+                @endif
+            </div>
         </div>
         @unless ($user->id === auth()->user()->id)
             <div class="flex items-center justify-end gap-4">
