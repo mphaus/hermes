00fa549fe3c47742ac61d7a00ab0c496d42c22bf
Arturo Guillén <aguillen.js@gmail.com>
2024-10-24 09:38:08 -0400

    Hidden main navigation menu.

--- resources/views/components/layout-app.blade.php
+++ resources/views/components/layout-app.blade.php
@@ -29,7 +29,7 @@
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
-            <x-app-navigation />
+            {{-- <x-app-navigation /> --}}
 
             <!-- Page Heading -->
             @if (isset($heading))
