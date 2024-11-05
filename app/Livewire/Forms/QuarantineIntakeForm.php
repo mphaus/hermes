26a34fef4609ab1edee9c8628446b276c8aad8d1
Arturo Guillén <aguillen.js@gmail.com>
2024-11-05 12:27:43 -0400

    QI: Started implementation of the Quarantine Intake form validation.

--- app/Livewire/Forms/QuarantineIntakeForm.php
+++ app/Livewire/Forms/QuarantineIntakeForm.php
@@ -13,9 +13,27 @@ class QuarantineIntakeForm extends Form
 
     public string $serial_number_status = 'serial-number-exists';
 
+    #[Validate]
     public string $serial_number;
 
     public int $product_id;
 
     public string $description;
+
+    private int $type = 1; // Damaged
+
+    private int $store = 1; // Head Office
+
+    private int $stock_type = 1; // Rental
+
+    private int $quantity_booked_in = 1;
+
+    private bool $open_ended = true;
+
+    public function rules(): array
+    {
+        return [];
+    }
+
+    public function store() {}
 }
