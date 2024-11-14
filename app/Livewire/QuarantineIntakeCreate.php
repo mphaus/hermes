5f1f58aa540c1420d27567cca90bbf19227dd27d
Arturo Guillén <aguillen.js@gmail.com>
2024-11-14 10:30:13 -0400

    QI: Fixed CSS styles of dropdown elements when submitting the Quarantine Intake form.

--- app/Livewire/QuarantineIntakeCreate.php
+++ app/Livewire/QuarantineIntakeCreate.php
@@ -42,6 +42,9 @@ class QuarantineIntakeCreate extends Component
 
     public function save()
     {
+        sleep(30);
+        return;
+
         if (!usercan('access-quarantine-intake')) {
             abort(403);
         }
