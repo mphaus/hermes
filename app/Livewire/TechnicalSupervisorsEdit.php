9c227142cf0717ca9fa94cc9d26c5a2358787fb7
Arturo Guillén <aguillen.js@gmail.com>
2024-11-18 12:21:01 -0400

    QI: Added the Tecnhical Supervisor edit form loading skeleton.

--- app/Livewire/TechnicalSupervisorsEdit.php
+++ app/Livewire/TechnicalSupervisorsEdit.php
@@ -3,10 +3,22 @@
 namespace App\Livewire;
 
 use Illuminate\Contracts\View\View;
+use Livewire\Attributes\Lazy;
 use Livewire\Component;
 
+#[Lazy]
 class TechnicalSupervisorsEdit extends Component
 {
+    public function mount()
+    {
+        sleep(5);
+    }
+
+    public function placeholder(): View
+    {
+        return view('technical-supervisors-edit-skeleton');
+    }
+
     public function render(): View
     {
         return view('livewire.technical-supervisors-edit');
---
+++ resources/views/technical-supervisors-edit-skeleton.blade.php
@@ -0,0 +1,25 @@
+<x-slot name="title">{{ __('Edit Technical Supervisor') }}</x-slot>
+<x-slot name="heading">
+    <span>{{ __('Edit Technical Supervisor name') }}</span>
+    <span class="block mt-2 text-sm font-normal">{{ __('Double check the spelling of the name') }}</span>
+</x-slot>
+<div>
+    <x-card class="max-w-screen-sm mx-auto flow animate-pulse">
+        <div class="h-6 bg-gray-200 rounded-lg"></div>
+        <div class="flow">
+            <div class="grid gap-4 md:grid-cols-2">
+                <div class="space-y-1">
+                    <div class="h-5 bg-gray-200 rounded-lg"></div>
+                    <div class="h-10 bg-gray-200 rounded-lg"></div>
+                </div>
+                <div class="space-y-1">
+                    <div class="h-5 bg-gray-200 rounded-lg"></div>
+                    <div class="h-10 bg-gray-200 rounded-lg"></div>
+                </div>
+            </div>
+            <div class="flex justify-end">
+                <div class="h-8 bg-gray-200 rounded-lg w-[4.313rem]"></div>
+            </div>
+        </div>
+    </x-card>
+</div>
