101dff6eb1348309b4aaad146f7d4821bbb3e97f
Arturo Guillén <aguillen.js@gmail.com>
2024-12-07 11:15:13 -0400

    Updated the Quarantine Intake form error message.

--- resources/views/livewire/quarantine-intake-create.blade.php
+++ resources/views/livewire/quarantine-intake-create.blade.php
@@ -12,11 +12,11 @@
             <x-quarantine-intake-form />
             @if ($alert)
                 <div @class([
-                    'p-4 font-semibold rounded-lg',
+                    'p-4 rounded-lg',
                     'bg-green-100 text-green-500' => $alert['type'] === 'success',
                     'bg-red-100 text-red-500' => $alert['type'] === 'error',
                 ])>
-                    <p>{!! $alert['message'] !!}</p>
+                    <div class="flow">{!! $alert['message'] !!}</div>
                 </div>    
             @endif
         </div>
