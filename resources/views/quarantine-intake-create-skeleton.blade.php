e18dee4b9dec5f7115894122326826193aba4239
Arturo Guillén <aguillen.js@gmail.com>
2024-12-07 11:01:25 -0400

    Updated the Quarantine Intake page loading skeleton layout.

--- resources/views/quarantine-intake-create-skeleton.blade.php
+++ resources/views/quarantine-intake-create-skeleton.blade.php
@@ -4,13 +4,17 @@
         <span>{{ __('Quarantine Intake') }}</span>
         <span class="block mt-2 text-sm font-normal">{!! __('Refer to the <a href=":url" target="_blank" rel="nofollow" title="Add item to Quarantine via Hermes section">Add item to Quarantine via Hermes section</a> of the Quarantine Intake Process for detailed instructions.', ['url' => 'https://mphaustralia.sharepoint.com/:w:/r/teams/MPHAdministration/Shared%20Documents/Process/01%20In%20development/Process_%20Repairs%20Quarantine%20intake.docx?d=wc450b4cdc2e84c758363390091b56915&csf=1&web=1&e=BkqZrw&nav=eyJoIjoiMjAwNzMzMjA1NyJ9']) !!}</span>
     </x-slot>
-    <x-card class="max-w-screen-md mx-auto space-y-7 animate-pulse">
-        <div class="h-6 bg-gray-200 rounded-lg"></div>
+    <div class="max-w-screen-md mx-auto space-y-7">
+        <div class="animate-pulse">
+            <div class="h-6 bg-gray-200 rounded-lg"></div>
+        </div>
         @for ($i = 0; $i < 3; $i++)
-            <div class="space-y-1">
-                <div class="h-5 bg-gray-200 rounded-lg"></div>
-                <div class="h-10 bg-gray-200 rounded-lg"></div>
-            </div>
+            <x-card class="animate-pulse">
+                <div class="space-y-1">
+                    <div class="h-5 bg-gray-200 rounded-lg"></div>
+                    <div class="h-10 bg-gray-200 rounded-lg"></div>
+                </div>
+            </x-card>
         @endfor
-    </x-card>
+        </div>
 </div>
