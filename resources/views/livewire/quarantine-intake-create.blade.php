2f03dda2f463ef1b255029df05e4b7df45e7773b
Arturo Guillén <aguillen.js@gmail.com>
2024-12-07 10:35:28 -0400

    Added info text under the heading of the Quarantine Intake page.

--- resources/views/livewire/quarantine-intake-create.blade.php
+++ resources/views/livewire/quarantine-intake-create.blade.php
@@ -1,6 +1,7 @@
 <x-slot name="title">{{ __('Quarantine Intake') }}</x-slot>
 <x-slot name="heading">
     <span>{{ __('Quarantine Intake') }}</span>
+    <span class="block mt-2 text-sm font-normal">{!! __('Refer to the <a href=":url" target="_blank" rel="nofollow" title="Add item to Quarantine via Hermes section">Add item to Quarantine via Hermes section</a> of the Quarantine Intake Process for detailed instructions.', ['url' => 'https://mphaustralia.sharepoint.com/:w:/r/teams/MPHAdministration/Shared%20Documents/Process/01%20In%20development/Process_%20Repairs%20Quarantine%20intake.docx?d=wc450b4cdc2e84c758363390091b56915&csf=1&web=1&e=BkqZrw&nav=eyJoIjoiMjAwNzMzMjA1NyJ9']) !!}</span>
 </x-slot>
 <div class="flow">
     @if (empty($this->technicalSupervisors))
