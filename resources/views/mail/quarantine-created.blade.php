d326ac7be321f9c13c53fcdfa1dba6d5f1d4cb63
Arturo Guillén <aguillen.js@gmail.com>
2024-12-23 09:35:34 -0400

    Updated the quarantine created email template.

--- resources/views/mail/quarantine-created.blade.php
+++ resources/views/mail/quarantine-created.blade.php
@@ -19,7 +19,7 @@ A new item was submitted to Quarantine via Hermes on {{ now()->format('d-M-Y') }
 
 See <a href="https://mphaustralia.current-rms.com/quarantines/{{ $quarantine['id'] }}" target="_blank" rel="nofollow">item's Quarantine page in CurrentRMS</a>.
 
-This item now needs to be checked for how its' Quarantining affects future Jobs. See <a href="https://mphaustralia.sharepoint.com/:w:/r/teams/MPHAdministration/Shared%20Documents/Process/Process_%20DWM%20Service,%20Repairs,%20Maintenance%20and%20Manufacturing%20Manager.docx?d=w95728782b244437aa958768a9078cfad&csf=1&web=1&e=K1bja5&nav=eyJoIjoiNTQyOTM4MTk5In0" target="_blank" rel="nofollow">"Determine if equipment is needed on upcoming Jobs" section of the Quarantine Intake process</a> for details on how to do this.
+This item now needs to be checked for how its' Quarantining affects future Jobs. See <a href="https://mphaustralia.sharepoint.com/:w:/r/teams/MPHAdministration/Shared%20Documents/Process/Process_%20DWM%20Service,%20Repairs,%20Maintenance%20and%20Manufacturing%20Manager.docx?d=w95728782b244437aa958768a9078cfad&csf=1&web=1&e=K1bja5&nav=eyJoIjoiNTQyOTM4MTk5In0" target="_blank" rel="nofollow">"Determine if equipment is needed on upcoming Jobs" section of the DWM SRRM Manager process</a> for details on how to do this.
 
 Then, work on the item needs to be prioritised accordingly on the <a href="https://planner.cloud.microsoft/webui/plan/w2pc0IJEcEugQYvOV1sGPMgAGyQQ?tid=6ab23282-f366-406e-aeed-bee8ded2a924" target="_blank" rel="nofollow">Quarantine Planner board</a>.
 
