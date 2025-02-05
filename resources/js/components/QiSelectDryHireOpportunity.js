50fb4aab23a4f8fb9f1db74541c265d2adc50279
Arturo Guillén <aguillen.js@gmail.com>
2025-02-05 11:37:49 -0400

    Made the Technical Supervisor info not show up when searching for Dry Hires using the QI form.

--- resources/js/components/QiSelectDryHireOpportunity.js
+++ resources/js/components/QiSelectDryHireOpportunity.js
@@ -80,7 +80,6 @@ export default function QiSelectDryHireOpportunity () {
                     }
 
                     this.value = value;
-                    this.$dispatch( 'hermes:select-opportunity-change', { ...currentData.find( data => data.id === value ) } );
                 } )
                 .on( 'select2:open', () => {
                     /** @type {HTMLInputElement} */
