a1e94ccb833f5138ce818105dd5d2fd7128e0749
Arturo Guillén <aguillen.js@gmail.com>
2025-03-11 12:19:35 -0400

    Advanced with the creation of a discussion comment that contains the current diff of an equipment import process.

--- app/Http/Controllers/OpportunityItemsController.php
+++ app/Http/Controllers/OpportunityItemsController.php
@@ -6,6 +6,7 @@ use App\Http\Requests\EquipmentImportRequest;
 use App\OpportunityItems;
 use App\UploadLog;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Http;
 
 class OpportunityItemsController extends Controller
@@ -63,6 +64,106 @@ class OpportunityItemsController extends Controller
             return response()->json([
                 'redirect_to' => route('jobs.show', ['id' => $opportunity['id']]),
             ]);
+        }
+
+        if ($diff) {
+            $response = Http::current()->withQueryParameters([
+                'q[discussable_id_eq]' => $opportunity_id,
+                'q[subject_cont]' => 'Job status',
+                'per_page' => 1,
+            ])->get('discussions');
+
+            if ($response->successful()) {
+                ['discussions' => $discussions] = $response->json();
+
+                /**
+                 * @var array $discussions
+                 */
+
+                if ($discussions) {
+                    $remark = __(':username did a new Equipment Import for this Job. There changes were;', ['username' => Auth::user()->username]);
+                    $remark .= PHP_EOL . PHP_EOL;
+                    $remark .= '<ul>';
+
+                    foreach ($diff as $action => $items) {
+                        if (empty($items)) {
+                            continue;
+                        }
+
+                        $count = count($items);
+
+                        switch ($action) {
+                            case 'reduced':
+                                $remark .= '<li>';
+                                $remark .= __(':count :product with <strong>:action</strong> counts', [
+                                    'count' => $count,
+                                    'product' => $count > 1 ? __('Products') : __('Product'),
+                                    'action' => $action
+                                ]);
+                                $remark .= '<ul>';
+
+                                foreach ($items as $item) {
+                                    $remark .= __('<li>Now, only :quantity x :item_name are required</li>', ['quantity' => $item['quantity'], 'item_name' => $item['item_name']]);
+                                }
+
+                                $remark .= '</ul>';
+                                $remark .= '</li>';
+                                break;
+                            case 'increased':
+                                $remark .= '<li>';
+                                $remark .= __(':count :product with <strong>:action</strong> counts', [
+                                    'count' => $count,
+                                    'product' => $count > 1 ? __('Products') : __('Product'),
+                                    'action' => $action
+                                ]);
+                                $remark .= '<ul>';
+
+                                foreach ($items as $item) {
+                                    $remark .= __('<li>:added_quantity x :item_name were added</li>', ['quantity' => $item['added_quantity'], 'item_name' => $item['item_name']]);
+                                }
+
+                                $remark .= '</ul>';
+                                $remark .= '</li>';
+                                break;
+                            case 'removed':
+                                $remark .= '<li>';
+                                $remark .= __(':count :product were <strong>:action</strong>', [
+                                    'count' => $count,
+                                    'product' => $count > 1 ? __('Products') : __('Product'),
+                                    'action' => $action
+                                ]);
+                                $remark .= '<ul>';
+
+                                foreach ($items as $item) {
+                                    $remark .= __('<li>No :item_name are required now</li>', ['item_name' => $item['item_name']]);
+                                }
+
+                                $remark .= '</ul>';
+                                $remark .= '</li>';
+                                break;
+                            case 'added':
+                                $remark .= '<li>';
+                                $remark .= __(':count :product were <strong>:action</strong>', [
+                                    'count' => $count,
+                                    'product' => $count > 1 ? __('Products') : __('Product'),
+                                    'action' => $action
+                                ]);
+                                $remark .= '<ul>';
+
+                                foreach ($items as $item) {
+                                    $remark .= __('<li>:quantity x :item_name have been added</li>', ['quantity' => $item['quantity'], 'item_name' => $item['item_name']]);
+                                }
+
+                                $remark .= '</ul>';
+                                $remark .= '</li>';
+                                break;
+                        }
+                    }
+
+                    $remark .= '</ul>';
+                    dd($remark);
+                }
+            }
         }
 
         $upload_log = new UploadLog($opportunity['id'], $log);
