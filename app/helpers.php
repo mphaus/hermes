bcd8afcadc48bb8f4d5fd96ca1eb42850baaa50c
Arturo Guillén <arturodevui@gmail.com>
2025-05-19 09:35:24 -0400

    Fixed route mappings when users log in.

--- app/helpers.php
+++ app/helpers.php
@@ -36,8 +36,9 @@ if (!function_exists('get_redirect_route')) {
             'access-qet' => route('qet.index'),
             'create-default-discussions' => route('discussions.create'),
             'update-default-discussions' => route('discussions.edit'),
-            'access-quarantine-intake' => route('quarantine-intake.create'),
-            'crud-technical-supervisors' => route('technical-supervisors.index'),
+            'access-quarantine-intake' => route('quarantine.create.view'),
+            'crud-production-administrators' => route('production-administrators.index.view'),
+            'crud-technical-supervisors' => route('technical-supervisors.index.view'),
             'crud-users' => route('users.index'),
         ];
 
