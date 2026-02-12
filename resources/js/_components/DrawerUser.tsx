b7058be79b077af7a220de4cfcb384a4520c76a4
Arturo Guillén <arturolguillen1@gmail.com>
2026-02-12 09:07:47 -0400

    feat: refactor logout functionality in DrawerUser component to use Link instead of Form

--- resources/js/_components/DrawerUser.tsx
+++ resources/js/_components/DrawerUser.tsx
@@ -1,18 +1,16 @@
 import { destroy } from "@/actions/App/Http/Controllers/InertiaAuthenticatedSessionController";
 import { SharedData } from "@/types";
-import { Form, usePage } from "@inertiajs/react";
+import { Form, Link, usePage } from "@inertiajs/react";
 
 export default function DrawerUser() {
     const { auth } = usePage<SharedData>().props;
 
     return (
         <div className="dropdown dropdown-top">
-            <div tabIndex={ 0 } role="button" className="cursor-pointer px-7 py-4 text-primary hover:text-base-content font-semibold">{ `${auth.user?.first_name} ${auth.user?.last_name}` }</div>
-            <ul tabIndex={ -1 } className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+            <div tabIndex={0} role="button" className="cursor-pointer px-7 py-4 text-primary hover:text-base-content font-semibold">{`${auth.user?.first_name} ${auth.user?.last_name}`}</div>
+            <ul tabIndex={-1} className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
                 <li>
-                    <Form action={ destroy() } method="POST" className="block">
-                        <button type="submit" className="block w-full cursor-pointer text-left">{ 'Logout' }</button>
-                    </Form>
+                    <Link href={destroy()} method="post">{'Logout'}</Link>
                 </li>
             </ul>
         </div>
