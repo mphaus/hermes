655ad549f1e48c06732b328e3835164e8d7b0e70
Arturo Guillén <arturolguillen1@gmail.com>
2026-02-18 17:16:30 -0400

    feat: enhance password validation in ResetPasswordForm component

--- resources/js/_components/ResetPasswordForm.tsx
+++ resources/js/_components/ResetPasswordForm.tsx
@@ -1,9 +1,61 @@
 import { Form } from "@inertiajs/react";
 import FormGroup from "./FormGroup";
+import clsx from "clsx";
+import { useMemo, useReducer, useRef } from "react";
+
+const lengthPattern = /.{16,24}/;
+const twoUppercaseLettersPattern = /(?=.*[A-Z].*[A-Z])/;
+const twoLowercaseLettersPattern = /(?=.*[a-z].*[a-z])/;
+const twoNumbersPattern = /(?=.*\d.*\d)/;
+const twoSpecialCharactersPattern = /(?=.*[!@#$%^&*].*[!@#$%^&*])/;
 
 export default function ResetPasswordForm({ token }: {
     token: string;
 }) {
+    const passwordRef = useRef<HTMLInputElement | null>(null);
+    const passwordConfirmationRef = useRef<HTMLInputElement | null>(null);
+    const [renderTick, forceRender] = useReducer((x) => x + 1, 0);
+
+    const password = passwordRef.current?.value ?? "";
+    const passwordConfirmation = passwordConfirmationRef.current?.value ?? "";
+    const passwordsMatch = password === passwordConfirmation;
+
+    const matchesAndHasValidLength = useMemo(() => {
+        return passwordsMatch && lengthPattern.test(password);
+    }, [passwordsMatch, password, renderTick]);
+
+    const matchesAndHasTwoUppercase = useMemo(() => {
+        return passwordsMatch && twoUppercaseLettersPattern.test(password);
+    }, [passwordsMatch, password, renderTick]);
+
+    const matchesAndHasTwoLowercase = useMemo(() => {
+        return passwordsMatch && twoLowercaseLettersPattern.test(password);
+    }, [passwordsMatch, password, renderTick]);
+
+    const matchesAndHasTwoNumbers = useMemo(() => {
+        return passwordsMatch && twoNumbersPattern.test(password);
+    }, [passwordsMatch, password, renderTick]);
+
+    const matchesAndHasTwoSpecialCharacters = useMemo(() => {
+        return passwordsMatch && twoSpecialCharactersPattern.test(password);
+    }, [passwordsMatch, password, renderTick]);
+
+    const isPasswordValid = useMemo(() => {
+        return (
+            matchesAndHasValidLength &&
+            matchesAndHasTwoUppercase &&
+            matchesAndHasTwoLowercase &&
+            matchesAndHasTwoNumbers &&
+            matchesAndHasTwoSpecialCharacters
+        );
+    }, [
+        matchesAndHasValidLength,
+        matchesAndHasTwoUppercase,
+        matchesAndHasTwoLowercase,
+        matchesAndHasTwoNumbers,
+        matchesAndHasTwoSpecialCharacters,
+    ]);
+
     return (
         <div className="bg-base-100 card shadow-sm">
             <div className="card-body">
@@ -37,6 +89,8 @@ export default function ResetPasswordForm({ token }: {
                                     id="password"
                                     className="input"
                                     required
+                                    ref={passwordRef}
+                                    onInput={forceRender}
                                 />
                             </FormGroup>
                             <FormGroup>
@@ -47,6 +101,8 @@ export default function ResetPasswordForm({ token }: {
                                     id="password_confirmation"
                                     className="input"
                                     required
+                                    ref={passwordConfirmationRef}
+                                    onInput={forceRender}
                                 />
                             </FormGroup>
                             <div className="space-y-4 text-xs">
@@ -54,14 +110,29 @@ export default function ResetPasswordForm({ token }: {
                                     <p>{'A unique and secure password is required.'}</p>
                                 </div>
                                 <ul className="pl-5 space-y-1 list-disc">
-                                    <li className="text-error">{'Minimum 16 characters'}</li>
+                                    <li className={clsx({
+                                        'text-error': !matchesAndHasValidLength,
+                                        'text-success': matchesAndHasValidLength,
+                                    })}>{'16 to 24 characters'}</li>
                                     <li>
                                         {'At least'}
                                         <ul className="list-[circle] pl-7 space-y-1 mt-1">
-                                            <li className="text-error">{'2 uppercase letters'}</li>
-                                            <li className="text-error">{'2 lowercase letters'}</li>
-                                            <li className="text-error">{'2 numbers'}</li>
-                                            <li className="text-error">{'2 special characters (!@#$%^&*()-_+=)'}</li>
+                                            <li className={clsx({
+                                                'text-error': !matchesAndHasTwoUppercase,
+                                                'text-success': matchesAndHasTwoUppercase,
+                                            })}>{'2 uppercase letters'}</li>
+                                            <li className={clsx({
+                                                'text-error': !matchesAndHasTwoLowercase,
+                                                'text-success': matchesAndHasTwoLowercase,
+                                            })}>{'2 lowercase letters'}</li>
+                                            <li className={clsx({
+                                                'text-error': !matchesAndHasTwoNumbers,
+                                                'text-success': matchesAndHasTwoNumbers,
+                                            })}>{'2 numbers'}</li>
+                                            <li className={clsx({
+                                                'text-error': !matchesAndHasTwoSpecialCharacters,
+                                                'text-success': matchesAndHasTwoSpecialCharacters,
+                                            })}>{'2 special characters (!@#$%^&*)'}</li>
                                         </ul>
                                     </li>
                                 </ul>
@@ -71,7 +142,7 @@ export default function ResetPasswordForm({ token }: {
                                 <button
                                     type="submit"
                                     className="btn btn-primary btn-block sm:w-auto"
-                                    disabled={processing}
+                                    disabled={processing || !isPasswordValid}
                                 >
                                     {processing ? 'Resetting...' : 'Reset Password'}
                                 </button>
