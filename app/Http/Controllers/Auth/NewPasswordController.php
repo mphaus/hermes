3c21675c30d424fc7836b064e9ff0849aa6f9c3b
Arturo Guillén <arturolguillen1@gmail.com>
2026-02-19 15:26:56 -0400

    refactor: remove unused RedirectResponse import in NewPasswordController

--- app/Http/Controllers/Auth/NewPasswordController.php
+++ app/Http/Controllers/Auth/NewPasswordController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Auth\Events\PasswordReset;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Password;
