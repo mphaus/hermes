5e7554df683d7eb12e565222c77f0767246f7794
Arturo Guillén <arturolguillen1@gmail.com>
2026-03-05 19:34:28 -0400

    feat: improve Technical Supervisor index loading experience
    - Replaced loading fallback with TechnicalSupervisorSkeleton for better user experience during data fetching.
    - Added informational alert for cases when no Technical Supervisors are present, enhancing user feedback.

--- resources/js/pages/TechnicalSupervisorIndex.tsx
+++ resources/js/pages/TechnicalSupervisorIndex.tsx
@@ -1,3 +1,4 @@
+import TechnicalSupervisorSkeleton from "@/_components/TechnicalSupervisorSkeleton";
 import TechnicalSupervisorsList from "@/_components/TechnicalSupervisorsList";
 import { CurrentRMSListValue, SharedData } from "@/types";
 import { Deferred, Head, usePage } from "@inertiajs/react";
@@ -13,13 +14,18 @@ export default function TechnicalSupervisorIndex() {
     return (
         <>
             <Head title={title} />
-            <Deferred data="technical_supervisors_data" fallback={<div>Loading...</div>}>
+            <Deferred data="technical_supervisors_data" fallback={<TechnicalSupervisorSkeleton />}>
                 {error && (
                     <div role="alert" className="alert alert-error">
                         {error}
                     </div>
                 )}
-                {technicalSupervisors && (
+                {!technicalSupervisors?.length && (
+                    <div role="alert" className="alert alert-info max-w-3xl mx-auto">
+                        {'You have not added any Technical Supervisors yet.'}
+                    </div>
+                )}
+                {!!technicalSupervisors?.length && (
                     <TechnicalSupervisorsList technicalSupervisors={technicalSupervisors} />
                 )}
             </Deferred>
