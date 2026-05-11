a39d22a1b40eb1120d899f9f9a086327b074987a
Arturo Guillén <arturolguillen1@gmail.com>
2026-05-11 11:59:47 -0400

    feat: refactor product mapping logic in label generation

--- app/Http/Controllers/ProductsLabelsGenerateController.php
+++ app/Http/Controllers/ProductsLabelsGenerateController.php
@@ -15,6 +15,18 @@ class ProductsLabelsGenerateController extends Controller
 
         ['products' => $products] = $validated;
 
+        $products = collect($products)->map(function (array $product) {
+            return [
+                'id' => $product['id'],
+                'name' => $product['name'],
+                'icon_url' => $product['icon']['url'] ?? 'https://placehold.co/600x600?text=No+image',
+                'colour_coded_storage' => $product['custom_fields']['colour_coded_storage'] ?? null,
+                'nally_bin_storage' => $product['custom_fields']['nally_bin_storage'] ?? null,
+                'nally_bin_storage_stored_at_height' => $product['custom_fields']['nally_bin_storage_stored_at_height'] ?? null,
+                'tub_storage' => $product['custom_fields']['tub_storage'] ?? null,
+            ];
+        });
+
         dd($products);
     }
 }
